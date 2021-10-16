20bd37b87b9551bfa3003c3fd738b95c3423d94c
FPorucznik <porucznik199@wp.pl>
2021-10-16 15:38:27 +0200

    Added movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YourMovies.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        [Display(Name ="Release Year")]
+        [Range(1900, 2100)]
+        public int ReleaseYear { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
